5fee16f21946d346909319f7194176b271d13fb4
Ali Mianaei <ali.mianaei@gmail.com>
2023-01-23 17:24:41 +0100

    fixes folder structure, routing, add bootstrap and font-awesome

---
+++ src/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+const Main = () => {
+  return (
+    <Routes>
+        <Route path='/' exact element={<Home/>} />
+    </Routes>
+  )
+}
+
+export default Main;
