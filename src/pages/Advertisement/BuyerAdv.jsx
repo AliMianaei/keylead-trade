65967382eb75bee3a3357f2988c2673eb5b6f7cf
Ali Mianaei <ali.mianaei@gmail.com>
2023-02-20 21:49:14 +0100

    fixes buyer form

--- src/pages/Advertisement/BuyerAdv.jsx
+++ src/pages/Advertisement/BuyerAdv.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AdvFilterBox from '../../components/AdvFilterBox'
 
 const BuyerAdv = () => {
+  const [bio, setBio] = useState(false);
   return (
-    <div style={{height:'300px'}}>Buyer</div>
+    <form action="" className=''>
+      <div className='sellerAdv'>
+        <AdvFilterBox title= 'Main Category'/>
+        <AdvFilterBox title= 'Type'/>
+        <AdvFilterBox title= 'Quantity'/>
+        <AdvFilterBox title= 'Country'/>
+        <div className="">
+          <label htmlFor="bio" className={`h-100 d-center pointer border-thistle py-3 ${bio ? 'white' : 'violaPurple'} ${bio ? 'bg-violaPurple' : 'bg-white'}`}>
+            <span className='me-2'>Bio</span>
+            <span className={`${bio ? 'd-inline' : 'd-none'}`}>&#10004;</span>
+          </label>
+          <input 
+            type="checkbox" 
+            name="bio" 
+            id="bio"
+            className='d-none' 
+            onChange={(e) => setBio(e.target.checked)} />
+        </div>
+      </div>
+      {/* <div className="d-end my-5">
+        <label htmlFor="bio" className={`bio-label d-center pointer border-thistle ${bio ? 'white' : 'violaPurple'} ${bio ? 'bg-violaPurple' : 'bg-white'}`}>
+          <span className='me-2'>Bio</span>
+          <span className={`${bio ? 'd-inline' : 'd-none'}`}>&#10004;</span>
+        </label>
+        <input 
+          type="checkbox" 
+          name="bio" 
+          id="bio"
+          className='d-none' 
+          onChange={(e) => setBio(e.target.checked)} />
+      </div> */}
+
+      
+      
+      <div className='text-end mt-5'>
+        <button className='custom-btn btn-rosyPink-fill px-5'>Send</button>
+      </div>
+    </form>
   )
 }
 
