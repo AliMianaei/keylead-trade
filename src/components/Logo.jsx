24e0fa9e837d3dd9fb1f43def4565b8177650d5a
Ali Mianaei <ali.mianaei@gmail.com>
2023-02-22 16:16:07 +0100

    fixes Logo link

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Logo = () => {
   return (
-    <div className='width-max-content'>
+    <Link to='/' className='width-max-content d-block text-decoration-none'>
         <h1 className='text-uppercase violaPurple mb-0 px-3' style={{borderBottom:'2px solid #6E467B'}}>keylead</h1>
-        <p className='mb-0' >Food Products Exchange</p>
-    </div>
+        <p className='mb-0 darkPurple' >Food Products Exchange</p>
+    </Link>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <ul className='list-unstyled d-flex  m-0 flex-nowrap text-center' style={{gap:'20px'}}>
-        <li className='py-1 pe-2 pointer' style={{minWidth:'max-content'}}>Products</li>
+        <li className='py-1 pe-2 pointer' style={{minWidth:'max-content'}}>
+          <Link to='/products' className="text-decoration-none haiti">Products</Link>
+        </li>
         <li className='py-1 px-2 pointer' style={{minWidth:'max-content'}}>Services</li>
         <li className='py-1 px-2 pointer' style={{minWidth:'max-content'}}>Blog</li>
         <li className='py-1 px-2 pointer' style={{minWidth:'max-content'}}>About us</li>
